f589b4a716d64b5ac88632346346c609c21e8827
Andhromede <andhromede@hotmail.fr>
2021-10-18 13:02:08 +0200

    maj

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+//Import CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './assets/css/general.css'
+
+//Import pages
 import { Home } from './pages/Home';
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
-
-import logo from "../assets/drapeaux/logomda.png";
+//Import CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MyItem from "../assets/css/item";
 
 import React, { useState } from "react";
+
+//Import components
 import Carousel from "react-elastic-carousel";
-import MyItem from "../assets/css/item";
 import { CustomBtnRightArrow } from "../components/CustomBtnRightArrow";
 import { CustomBtnLeftArrow } from "../components/CustomBtnLeftArrow";
 import { CustomCarousel } from "../components/CustomCarousel";
+
+
+
 
 export function Home() {
 
