e7a0542e35be8edcfcbd853702bdc0ce9a43265c
Andhromede <andhromede@hotmail.fr>
2021-11-02 16:14:42 +0100

    maj page login (back)

--- Site/src/pages/Login.jsx
+++ Site/src/pages/Login.jsx
@@ -2,10 +2,42 @@ import { Navbar } from '../components/Navbar.jsx';
 import '../assets/css/general.css';
 import '../assets/css/login.css';
 import { BsFillPersonFill} from 'react-icons/bs'
-import { AiFillLock} from 'react-icons/ai'
+import { AiFillLock, AiOutlineRetweet} from 'react-icons/ai'
+
+import { useState, useEffect } from 'react';
+import  Axios from 'axios';
 
 
 export function Login() {
+
+    const [mail, setMail] = useState([]);
+    const [password, setPassword] = useState([]);
+
+    //CONNEXION
+    const onClick = () => {
+        Axios.get(`http://10.115.58.226:3001/api/connexion/${mail}`).then((response) => {
+            setMail(response.data);
+            let result = response.data[0];
+            console.log(result);
+            
+            // if(response.data.length === 0 ){
+            //     alert("cet email n'existe pas");
+            // }else if(result.mail === mail && result.mdp !== password){
+            //     alert("mdp incorect");
+            // }else{
+            //     alert("connexion ok");
+            // }
+        });
+    }
+    
+    //CONNEXION
+    // const onClick = () => {
+    //     Axios.get(`http://10.115.58.226:3001/api/connexion/${mail}`).then((response) => {
+    //         setMail(response.data);
+    //     });
+    // }
+
+
 
     return (
         <>
@@ -14,7 +46,7 @@ export function Login() {
                 <div className="container h-100">
                     <div className="d-flex justify-content-center h-100">
                         <div className="user_card">
-                            <h1 className="fw-bolder text-light">Connexion</h1>
+                            <h1 className="fw-bolder text-light mt-2">Connexion</h1>
 
                             <div className="d-flex justify-content-center form_container">
                                 <form>
@@ -23,7 +55,7 @@ export function Login() {
                                         <div className="input-group-append">
                                             <span className="input-group-text"><BsFillPersonFill className="icones mt-15px w-75 h-75 my-auto mx-auto"></BsFillPersonFill></span> 
                                         </div>
-                                        <input type="text" name="" className="form-control input_user" value="" placeholder="username"/>
+                                        <input onChange={(e)=>{setMail(e.target.value);}} type="text" className="form-control input_user" placeholder="email"/>
                                     </div>
 
                                     {/* PASSWORD */}
@@ -31,11 +63,12 @@ export function Login() {
                                         <div className="input-group-append">
                                             <span className="input-group-text"><AiFillLock className="icones mt-15px w-75 h-75 my-auto mx-auto"></AiFillLock></span> 
                                         </div>
-                                        <input type="password" name="" className="form-control input_pass" value="" placeholder="password"/>
+                                        <input onChange={(e)=>{setPassword(e.target.value);}} type="password" name="password" className="form-control input_pass" placeholder="password"/>
                                     </div>
 
                                     {/* REMEMBER */}
                                     <div className="form-group mt-5">
+                                    <button onClick={onClick} type="button" name="button" className="btn text-light" route="Inscription">Inscription</button>
                                         {/* <div className="custom-control custom-checkbox">
                                             <input type="checkbox" className="custom-control-input" id="customControlInline"/>
                                             <label className ="custom-control-label" >Remember me</label>
@@ -43,8 +76,8 @@ export function Login() {
                                     </div>
 
                                     {/* BUTTON LOGIN */}
-                                    <div className="d-flex justify-content-center mt-3 login_container">
-                                        <button type="button" name="button" className="btn login_btn">Connexion</button>
+                                    <div className="d-flex justify-content-center my-3 login_container">
+                                        <button onClick={onClick} type="button" name="button" className="btn login_btn">Connexion</button>
                                     </div>
 
                                 </form>
